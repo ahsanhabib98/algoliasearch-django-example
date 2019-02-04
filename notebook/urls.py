4ae5e06d0acf239d9d65a0bed53fdbaa263422f2
ahsanhabib98 <ahredoan@gmail.com>
2019-02-04 15:08:25 +0600

    modified

--- notebook/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^auto-complete$', views.auto_complete, name='auto_complete'),
-    url(r'^instant-search$', views.instant_search, name='instant_search')
-]
